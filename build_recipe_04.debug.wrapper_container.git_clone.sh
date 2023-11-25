cd3ceace1d5e8961bf2d1678abb6712909beb4b5
al <no-replay@github.com>
2023-11-24 21:18:43 -0800

    v5 debug container with inner git clone

--- build_recipe_04.debug.wrapper_container.git_clone.sh
+++ build_recipe_04.debug.wrapper_container.git_clone.sh
@@ -9,6 +9,10 @@ docker build -f ./Dockerfile.04-call-python-main-src-git-clone -t ${BASEIMAGE_TO
 # Inject the parent docker image to debug into the derived debugging image
 docker build -t ${WRAPPER_IMAGE} --build-arg BASEIMAGE=${BASEIMAGE_TO_DEBUG} -f Dockerfile.05-debug-image-as-base-image .
 
+# Prepare out-of-container debug code
+rm -rf debug
+git clone https://github.com/alicata/build-debug.git debug
+
 # Start debug server with main.app
 # Debug server waits for debug client (VSCode GUI) to startgit 
 # User selects Start-Debug with Remote-Attach config
@@ -16,5 +20,5 @@ echo start debugging image that launches the debug server and waits for vscode d
 echo ${WRAPPER_IMAGE}: debug server starting ...
 echo ${WRAPPER_IMAGE}: debug server waiting for VSCode client ...
 echo ${WRAPPER_IMAGE}: please start Debug in VSCode ...
-docker run -it -p 5678:5678  ${WRAPPER_IMAGE}
+docker run -it --rm -p 5678:5678 -v debug:/workspace/picospace/debug  ${WRAPPER_IMAGE}
 echo ${WRAPPER_IMAGE}: done.
